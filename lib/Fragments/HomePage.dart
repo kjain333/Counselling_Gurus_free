5358f795a4ce283159536850fb8e5320cacb2741
kjain333 <karanjain140701@gmail.com>
2020-05-08 18:13:01 +0530

    home page updated

--- lib/Fragments/HomePage.dart
+++ lib/Fragments/HomePage.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,7 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
 
   List<String> cardHeadings = ["Colleges", "Branches", "Mock Counselling", "Aptitude Test"];
-
+  List<IconData> icon = [Icons.home,Icons.library_books,Icons.supervisor_account,Icons.edit];
+  String paragraph = "These are few lines describing each card to be displayed on the back.\nHere is some more random text so that the button can reach the botttom";
   @override
   Widget build(BuildContext context) {
 
@@ -52,10 +54,10 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                 child: GridView(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: (itemWidth / itemHeight),),
                   children: <Widget>[
-                    gridCard(cardHeadings[0]),
-                    gridCard(cardHeadings[1]),
-                    gridCard(cardHeadings[2]),
-                    gridCard(cardHeadings[3]),
+                    gridCard(cardHeadings[0],icon[0],paragraph),
+                    gridCard(cardHeadings[1],icon[1],paragraph),
+                    gridCard(cardHeadings[2],icon[2],paragraph),
+                    gridCard(cardHeadings[3],icon[3],paragraph),
                   ],
                 ),
               ),
@@ -66,7 +68,7 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     );
   }
 
-  Widget gridCard(cardHeading){
+  Widget gridCard(cardHeading,icon,para){
 
     return Padding(
       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
@@ -75,64 +77,116 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
           front: Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(20)),
-              side: BorderSide(width: 1, color: Colors.black),
+              //side: BorderSide(width: 1, color: Colors.black),
             ),
             color: Colors.white,
             elevation: 10,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(15, 15, 15, 8),
-              child: Stack(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.deepOrangeAccent,Colors.orangeAccent]
+                )
+              ),
+              child: Column(
                 children: <Widget>[
-                  Center(
-                      child: Text(cardHeading, style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold
-                      ),)
+                  SizedBox(
+                    height: 60,
                   ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Text("Tap to get more info", style: TextStyle(
-                        fontSize: 10,
-                        color: Colors.grey
-                    ),),
-                  )
+                  Icon(
+                      icon,size: 60,color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  ListTile(
+                    title: Center(child: Text(cardHeading,style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),),),
+                    subtitle: Text('Here we will put some lines of predefined text',style: GoogleFonts.aBeeZee(fontWeight: FontWeight.w300,fontSize: 13,color: Colors.white),),
+                  ),
+
+
                 ],
               ),
-            ),
+
+            )
           ),
           back: Card(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(20)),
-              side: BorderSide(width: 1, color: Colors.black),
+             // side: BorderSide(width: 1, color: Colors.black),
             ),
             color: Colors.yellow,
             elevation: 10,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(15, 15, 15, 8),
-              child: Stack(
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.blueAccent,Colors.lightBlueAccent]
+                  )
+              ),
+              child: Column(
                 children: <Widget>[
-                  Center(
-                      child: RaisedButton(
-                        child: Text("Get a list of colleges" , style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold
-                        ),),
-                        onPressed: () {
-
-                        },
-                      )
+                  SizedBox(
+                    height: 30
                   ),
+
+                  Padding(
+                    padding: EdgeInsets.only(left:20,right: 20),
+                    child: SizedBox(
+                      // title: Center(child: Text(cardHeading,style: GoogleFonts.aBeeZee(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),),),
+                      child: Text(para,style: GoogleFonts.aBeeZee(fontWeight: FontWeight.w300,fontSize: 15,color: Colors.white),),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(30),
+                      child: RaisedButton(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        color: Colors.purpleAccent,
+                        child: Text('MORE DETAILS',style: GoogleFonts.aBeeZee(color: Colors.white,fontSize: 10),),
+                        onPressed: (){return null;}
+                      ),
+
+                  )
+
+
                 ],
-            ),
-          ),
+              ),
+
+            )
         ),
       ),
     );
   }
 
 }
+/*Padding(
+padding: EdgeInsets.fromLTRB(15, 15, 15, 8),
+child: Stack(
+children: <Widget>[
+Center(
+child: Text(cardHeading, style: TextStyle(
+fontSize: 20,
+color: Colors.black,
+fontWeight: FontWeight.bold
+),)
+),
+Align(
+alignment: Alignment.bottomRight,
+child: Text("Tap to get more info", style: TextStyle(
+fontSize: 10,
+color: Colors.grey
+),),
+)
+],
+),
+),*/
 
 class CircleImages extends StatefulWidget {
   @override
