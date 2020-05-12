c9f431d65bd404a26d795b6166eadf25b2391379
kjain333 <karanjain140701@gmail.com>
2020-05-12 17:01:24 +0530

    mentor login edited

--- lib/Pages/Mentor/MentorInfo.dart
+++ lib/Pages/Mentor/MentorInfo.dart
@@ -12,14 +12,14 @@ class MentorInfo extends StatelessWidget{
         children: <Widget>[
           Container(
             width: double.infinity,
-            height: 250,
+            height: 180,
             decoration: BoxDecoration(
                 gradient: LinearGradient(begin: Alignment.topCenter, colors: [
                   Colors.orange[900],
                   Colors.orange[800],
                   Colors.orange[400]
                 ])),
-            child: Text('\n\n\n\n\n  WELCOME BACK',style: TextStyle(color: Colors.white,fontSize: 18),),
+            child: Text('\n\n\n  WELCOME BACK',style: TextStyle(color: Colors.white,fontSize: 18),),
           ),
           Material(
             shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: Radius.circular(50),topRight: Radius.circular(50))),
@@ -35,10 +35,12 @@ class MentorInfo extends StatelessWidget{
                 Padding(
                   padding: EdgeInsets.only(left: 30,right: 30),
                   child: Material(
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(20),topLeft: Radius.circular(20)),
                     elevation: 20,
                     child: Container(
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
+                         // borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
                           border: Border(bottom: BorderSide(color: Colors.grey[200]))
                       ),
                       child: TextField(
@@ -73,24 +75,29 @@ class MentorInfo extends StatelessWidget{
                 Padding(
                   padding: EdgeInsets.only(left: 30,right: 30),
                   child: Material(
+                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20)),
                     elevation: 20,
                     child: Container(
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
+                        //  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
                           border: Border(bottom: BorderSide(color: Colors.grey[200]))
                       ),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            hintText: "STUDYING YEAR",
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: InputBorder.none
-                        ),
-                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            width: double.infinity,
+                            height: 50,
+                            child: Text('STUDYING YEAR',style: TextStyle(color: Colors.grey),),
+                          ),
+                          RadioGroup()
+                        ],
+                      )
                     ),
                   ),
                 ),
                 SizedBox(
-                  height: 80,
+                  height: 30,
                 ),
                 RaisedButton(
                   child: Text('SUBMIT',style: TextStyle(color: Colors.white),),
@@ -104,6 +111,32 @@ class MentorInfo extends StatelessWidget{
           )
         ],
       ),
+    );
+  }
+
+
+}
+class RadioGroup extends StatefulWidget{
+  @override
+  RadioGroupWidget createState() {
+    return RadioGroupWidget();
+  }
+
+}
+
+class RadioGroupWidget extends State{
+  final index = [1,2,3,4,5];
+  int val;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RadioListTile(title: Text('Fresher'), groupValue: val, onChanged: (index) => setState((){ val=1;}),value: index[0],),
+        RadioListTile(title: Text('Sophomore'), groupValue: val, onChanged: (index) => setState(() => val=2),value: index[1],),
+        RadioListTile(title: Text('Pre-Final Year'), groupValue: val, onChanged: (index) => setState(() => val=3),value: index[2],),
+        RadioListTile(title: Text('Final Year'), groupValue: val, onChanged: (index) => setState(() => val=4),value: index[3],),
+        RadioListTile(title: Text('Passout'), groupValue: val, onChanged: (index) => setState(() => val=5),value: index[4],),
+      ],
     );
   }
 
