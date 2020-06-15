a9b6470d0c5dbafde35c3682b874521c7ef94788
kjain333 <karanjain140701@gmail.com>
2020-06-15 18:19:16 +0530

    search bar added

--- lib/Pages/Mentor/Medical/MedicalBranchName.dart
+++ lib/Pages/Mentor/Medical/MedicalBranchName.dart
@@ -1,3 +1,4 @@
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,6 +15,15 @@ final string6 = ['Communication','Electrical','Electronics\nand\nCommunication',
 final string7 = ['Computer\nScience\nEngineering','Information\nTechnology','Robotics','Aeronautical','Aerospace','Automobile','Transport'];
 final string8 = ['Ceramic','Civil','Construction','Structural\nEngineering'];
 final string = string1+string2+string3+string4+string5+string6+string7+string8;
+final string10 = ['Chemical Engineering','Chemistry','Engineering Physics','Environment','Oil','Petroleum'];
+final string20 = ['Mechanical','Mechatronics','Textile and Chemistry','Textile Engineering','Textile Technology'];
+final string30 = ['Industrial Engineering','Industrial and Production','Manufacturing','Metallurgy','Mining','Production'];
+final string40 = ['Instrumentation','Leather Technology','Man Made Fibre','Marine','Naval and Ocean','Plastic','Paint'];
+final string50 = ['Agriculture','Food','Biochemical','Biomedical','Biotechnology'];
+final string60 = ['Communication','Electrical','Electronics and Communication','Electronics and Instrumentation','Electronics and Tele Communications'];
+final string70 = ['Computer Science Engineering','Information Technology','Robotics','Aeronautical','Aerospace','Automobile','Transport'];
+final string80 = ['Ceramic','Civil','Construction','Structural Engineering'];
+final stringfinal = string10+string20+string30+string40+string50+string60+string70+string80;
 enum _BgProps{color1,color2}
 
 class MedicalBranchName extends StatefulWidget{
@@ -24,11 +34,27 @@ class MedicalBranchName extends StatefulWidget{
 }
 
 class _MedicalBranchNameState extends State<MedicalBranchName> {
+  TextEditingController controller = new TextEditingController();
+  ScrollController _controller;
+  final key = new GlobalKey<AutoCompleteTextFieldState<String>>();
+  @override
+  void initState(){
+    _controller  = ScrollController();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final tween = MultiTween<_BgProps>()..add(_BgProps.color1,Color(0xffD38312).tweenTo(Colors.lightBlue.shade900))
       ..add(_BgProps.color2,Color(0xffA83279).tweenTo(Colors.blue.shade600));
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_upward,color: Colors.white,),
+        onPressed: (){
+          setState(() {
+            _controller.animateTo(0, duration: Duration(seconds: 1), curve: Curves.bounceOut);
+          });
+        },
+      ),
       body: Stack(
           children: <Widget>[
             Container(
@@ -54,9 +80,66 @@ class _MedicalBranchNameState extends State<MedicalBranchName> {
               ),
             ),
             ListView.builder(
-              itemCount: string.length,
+              controller: _controller,
+              itemCount: string.length+1,
               itemBuilder: (BuildContext context,int index){
-                if(index%2==0)
+                if(index==0)
+                  return Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.white,
+                        ),
+                        child: AutoCompleteTextField<String>(
+                          key: key,
+                          controller: controller,
+                          /*     textChanged: (stringfinal){
+                          controller.text=stringfinal;
+                        },*/
+                          clearOnSubmit: false,
+                          suggestions: stringfinal,
+                          itemFilter: (item,query){
+                            return item.toLowerCase().startsWith(query.toLowerCase());
+                          },
+                          itemSorter: (a,b){
+                            return a.compareTo(b);
+                          },
+                          itemSubmitted: (stringitem){
+                            setState(() {
+                              controller.text=stringitem;
+                              int i;
+                              for(i=0;i<stringfinal.length;i++)
+                              {
+                                if(stringitem.compareTo(stringfinal[i])==0)
+                                  break;
+                              }
+                              _controller.animateTo((60+i*150).toDouble(), duration: Duration(seconds: 1), curve: Curves.linear);
+                            });
+                          },
+                          itemBuilder: (context,item){
+                            return text(item);
+                          },
+                          style: GoogleFonts.aBeeZee(fontWeight: FontWeight.w300,fontSize: 18),
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            focusColor: Colors.white,
+                            hoverColor: Colors.white,
+                            prefixIcon: Icon(Icons.search),
+                            hintText: 'Search',
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      )
+                  );
+                else if(index%2==1)
                   return Padding(
                       padding: EdgeInsets.only(right: 200),
                       child: GestureDetector(
@@ -72,7 +155,7 @@ class _MedicalBranchNameState extends State<MedicalBranchName> {
                                   colors: [Colors.deepOrange,Colors.orangeAccent],
                                 )
                             ),
-                            child:Center(child: Text(string[index],style: GoogleFonts.aBeeZee(fontSize: 12,color: Colors.white),),)
+                            child:Center(child: Text(string[index-1],style: GoogleFonts.aBeeZee(fontSize: 12,color: Colors.white),),)
                         ),
                       )
                   );
@@ -90,7 +173,7 @@ class _MedicalBranchNameState extends State<MedicalBranchName> {
                                   colors: [Colors.purple,Colors.pinkAccent],
                                 )
                             ),
-                            child:Center(child: Text(string[index],style: GoogleFonts.aBeeZee(fontSize: 12,color: Colors.white),),)
+                            child:Center(child: Text(string[index-1],style: GoogleFonts.aBeeZee(fontSize: 12,color: Colors.white),),)
                         ),
                       )
                   );
@@ -100,4 +183,21 @@ class _MedicalBranchNameState extends State<MedicalBranchName> {
       ),
     );
   }
+}
+Widget text(String item)
+{
+  return Column(
+    children: <Widget>[
+      Text(
+        item,style: GoogleFonts.aBeeZee(fontSize: 20,fontWeight: FontWeight.w300),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      Container(
+        height: 1,
+        color: Colors.grey,
+      )
+    ],
+  );
 }
--- lib/Pages/Student/HomePageSources/CollegeName.dart
+++ lib/Pages/Student/HomePageSources/CollegeName.dart
@@ -1,16 +1,36 @@
+import 'package:autocomplete_textfield/autocomplete_textfield.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../../Resources/Colors.dart' as color;
 
-class CollegeName extends StatelessWidget {
-
-  CollegeName(this.collegeLists);
+class CollegeName extends StatefulWidget{
+  CollegeName(this.collegelists);
+  final List<String> collegelists;
+  _CollegeName createState() => _CollegeName(collegelists);
+}
+class _CollegeName extends State<CollegeName>{
+  _CollegeName(this.collegeLists);
   final List<String> collegeLists;
-
+  final key = new GlobalKey<AutoCompleteTextFieldState<String>>();
+  TextEditingController controller = new TextEditingController();
+  ScrollController _controller;
+  @override
+  void initState(){
+    _controller  = ScrollController();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_upward,color: Colors.white,),
+        onPressed: (){
+          setState(() {
+            _controller.animateTo(0, duration: Duration(seconds: 1), curve: Curves.bounceOut);
+          });
+        },
+      ),
       backgroundColor: Color(0xfff0f0f0),
       body: SingleChildScrollView(
         child: Container(
@@ -23,9 +43,67 @@ class CollegeName extends StatelessWidget {
                 height: MediaQuery.of(context).size.height,
                 width: double.infinity,
                 child: ListView.builder(
-                    itemCount: collegeLists.length,
+                    controller: _controller,
+                    itemCount: collegeLists.length+1,
                     itemBuilder: (BuildContext context, int index) {
-                      return buildList(context, index);
+                      if(index == 0)
+                        return Padding(
+                            padding: EdgeInsets.all(30),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.white,
+                              ),
+                              child: AutoCompleteTextField<String>(
+                                controller: controller,
+                                key: key,
+                                /*     textChanged: (stringfinal){
+                          controller.text=stringfinal;
+                        },*/
+                                clearOnSubmit: false,
+                                suggestions: collegeLists,
+                                itemFilter: (item,query){
+                                  return item.toLowerCase().startsWith(query.toLowerCase());
+                                },
+                                itemSorter: (a,b){
+                                  return a.compareTo(b);
+                                },
+                                itemSubmitted: (stringitem){
+                                  setState(() {
+                                    controller.text=stringitem;
+                                    int i;
+                                    for(i=0;i<collegeLists.length;i++)
+                                    {
+                                      if(stringitem.compareTo(collegeLists[i])==0)
+                                        break;
+                                    }
+                                    _controller.animateTo((120+i*160).toDouble(), duration: Duration(seconds: 1), curve: Curves.linear);
+                                  });
+                                },
+                                itemBuilder: (context,item){
+                                  return text(item);
+                                },
+                                style: GoogleFonts.aBeeZee(fontWeight: FontWeight.w300,fontSize: 18),
+                                decoration: InputDecoration(
+                                  fillColor: Colors.white,
+                                  focusColor: Colors.white,
+                                  hoverColor: Colors.white,
+                                  prefixIcon: Icon(Icons.search),
+                                  hintText: 'Search',
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(color: Colors.black),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                    borderSide: BorderSide(color: Colors.black),
+                                  ),
+                                ),
+                              ),
+                            )
+                        );
+                      else
+                      return buildList(context, index-1);
                     }),
               ),
               Container(
@@ -135,4 +213,21 @@ class CollegeName extends StatelessWidget {
       ),
     );
   }
+}
+Widget text(String item)
+{
+  return Column(
+    children: <Widget>[
+      Text(
+        item,style: GoogleFonts.aBeeZee(fontSize: 20,fontWeight: FontWeight.w300),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      Container(
+        height: 1,
+        color: Colors.grey,
+      )
+    ],
+  );
 }
