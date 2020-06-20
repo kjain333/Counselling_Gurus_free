88e0195315705c239ab6373cb011e18a2697fc2a
Naman Agarwal <f20180435@pilani.bits-pilani.ac.in>
2020-06-20 15:46:29 +0530

    splash screen done

--- lib/Pages/SplashScreen.dart
+++ lib/Pages/SplashScreen.dart
@@ -8,23 +8,81 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  startTime() async {
+    var _duration = new Duration(seconds: 5);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginOptionsPage()));
+  //  Navigator.of(context).pushReplacementNamed('/LoginOptionsPage');
+  }
+
   @override
   void initState() {
     super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginOptionsPage()))
-    );
-
+    startTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return new Scaffold(
       body: Center(
-        child: Image.asset('assets/images/splashpic.png'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/splashpic.png',
+                scale: 0.8,),
+            SizedBox(height: 5.0),
+            Text(
+              "Counselling Gurus",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w600),
+            ),
+
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+//  @override
+//  void initState() {
+//    super.initState();
+//    Timer(
+//        Duration(seconds: 5),
+//        () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginOptionsPage()))
+//    );
+//
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Colors.white,
+//      body: Center(
+//        child: Column(
+//          children: <Widget>[
+//            Image.asset('assets/images/splashpic.png'),
+//            SizedBox(height: 5.0),
+//            Text(
+//              "Counselling Gurus",
+//              style: TextStyle(
+//                  color: Colors.black,
+//                  fontSize: 30.0,
+//                  fontWeight: FontWeight.w600),
+//            ),
+//
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
